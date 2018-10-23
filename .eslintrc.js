c16d3815e678a000416dc4d1ca023e7128534c73
Joseph <mmx362003@gmail.com>
2018-10-23 11:52:10 +0800

    fix eslint issue

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
   rules: {
     semi: [2, 'always'],
     'semi-spacing': [
-      4,
+      0,
       {
         before: false,
         after: true
@@ -14,6 +14,11 @@ module.exports = {
     'linebreak-style': 0,
     'padded-blocks': ['error', 'always'],
     'eol-last': ['error', 'always'],
-    'comma-style': ['error', 'last']
+    'comma-style': ['error', 'last'],
+    'no-multiple-empty-lines': ['error', {
+      'max': 1,
+      "maxEOF": 1,
+      "maxBOF": 1
+    }]
   }
 };
